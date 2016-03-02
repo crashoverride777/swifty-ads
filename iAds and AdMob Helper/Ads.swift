52813f51531b732411ce62dbd997079395ca953a
Dominik Ringler <dom.01@icloud.com>
2016-03-02 15:16:00 +0000

    v3.5.1

--- iAds and AdMob Helper/Ads.swift
+++ iAds and AdMob Helper/Ads.swift
@@ -23,7 +23,7 @@
 
 //    Dont forget to add the custom "-D DEBUG" flag in Targets -> BuildSettings -> SwiftCompiler-CustomFlags -> DEBUG)
 
-//    v3.5
+//    v3.5.1
 
 import iAd
 import GoogleMobileAds
