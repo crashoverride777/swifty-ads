3ec6066b67e669b30aff7298d933dd1ef581c3d8
Dom <Dominik@Doms-MacBook.local>
2017-02-24 19:18:30 +0000

    v7.0.2

--- SwiftyAds/SwiftyAds.swift
+++ SwiftyAds/SwiftyAds.swift
@@ -1,7 +1,6 @@
-
-//  Created by Dominik on 22/08/2015.
-
 //    The MIT License (MIT)
+//
+//    Copyright (c) 2015-2017 Dominik Ringler
 //
 //    Permission is hereby granted, free of charge, to any person obtaining a copy
 //    of this software and associated documentation files (the "Software"), to deal
