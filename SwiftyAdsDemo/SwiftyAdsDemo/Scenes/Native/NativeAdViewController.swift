3daf4f8ce75b4c393c1009b85798fc0316cf0fc3
Dominik Ringler <dom.01@icloud.com>
2021-02-13 11:13:00 +0100

    Fix native ad crash

--- SwiftyAdsDemo/SwiftyAdsDemo/Scenes/Native/NativeAdViewController.swift
+++ SwiftyAdsDemo/SwiftyAdsDemo/Scenes/Native/NativeAdViewController.swift
@@ -51,7 +51,8 @@ private extension NativeAdViewController {
 
     func showNativeAd(_ nativeAd: GADNativeAd) {
         // Create and place ad in view hierarchy.
-        let nibView = Bundle.main.loadNibNamed("NativeAdView", owner: nil, options: nil)?.first
+        let bundle = Bundle(for: NativeAdViewController.self)
+        let nibView = bundle.loadNibNamed("CustomAdView", owner: nil, options: nil)?.first
 
         guard let nativeAdView = nibView as? GADNativeAdView else {
             fatalError("NativeAdViewController could not create GADNativeAdView from nib")
