d277b0d3d9971ff12ddc641164bd66e3cbdc0449
Dominik Ringler <dom.01@icloud.com>
2021-03-07 20:59:14 +0100

    Test files

---
+++ Tests/Internal/SwiftyAdsConfigurationTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import SwiftyAds
+
+class SwiftyAdsConfigurationTests: XCTestCase {
+
+    // MARK: - Tests
+
+    func testDebug_isUMPConsentEnabled() {
+
+    }
+
+    func testDebug_isUMPConsentDisabled() {
+
+    }
+}
