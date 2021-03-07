c5f2f4f31c0a5e53f027b713f91f688c18f5acf2
Dominik Ringler <dom.01@icloud.com>
2021-03-07 19:42:09 +0100

    Update AppDelegate.swift

--- Demos/SpriteKit/SwiftyAdsSpriteKitDemo/AppDelegate.swift
+++ Demos/SpriteKit/SwiftyAdsSpriteKitDemo/AppDelegate.swift
@@ -7,7 +7,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     private let swiftyAds: SwiftyAdsType = SwiftyAds.shared
-    private let notificationCenter: NotificationCenter = .default
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
